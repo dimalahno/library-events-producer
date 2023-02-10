4eafd82a35874a7897990e9c9f97e8992292d67c
dmitriy.lakhno <dimalahno@rambler.ru>
2023-02-10 19:13:32 +0500

    Fix producer

--- src/main/java/com/learnkafka/producer/LibraryClassProducer.java
+++ src/main/java/com/learnkafka/producer/LibraryClassProducer.java
@@ -6,10 +6,13 @@ import com.learnkafka.domain.LibraryEvent;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @Component
@@ -47,13 +50,16 @@ public class LibraryClassProducer {
     }
 
     private ProducerRecord<Integer, String> buildProducerRecord(Integer key, String value, String topic) {
-        return new ProducerRecord<>(topic, null, key, value);
+
+        List<Header> recordsHeaders = List.of(new RecordHeader("event-source", "scanner".getBytes()));
+
+        return new ProducerRecord<>(topic, null, key, value, recordsHeaders);
     }
 
     public SendResult<Integer, String> sendLibraryEventSynchronous(LibraryEvent libraryEvent) throws JsonProcessingException {
         var key = libraryEvent.getLibraryEventId();
         var value = mapper.writeValueAsString(libraryEvent);
-        SendResult<Integer, String> sendResult = null;
+        SendResult<Integer, String> sendResult;
         try {
             sendResult = kafkaTemplate.sendDefault(key, value).get();
         } catch (InterruptedException | ExecutionException e) {

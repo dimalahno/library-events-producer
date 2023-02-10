53746f4f4c23903cdcfe3aa43649938e58c34990
dmitriy.lakhno <dimalahno@rambler.ru>
2023-02-10 19:12:09 +0500

    Fix controller

--- src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -2,17 +2,15 @@ package com.learnkafka.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
+import com.learnkafka.domain.LibraryEventType;
 import com.learnkafka.producer.LibraryClassProducer;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.ExecutionException;
 
 @RestController
 @AllArgsConstructor
@@ -22,14 +20,12 @@ public class LibraryEventsController {
     private final LibraryClassProducer producer;
 
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws ExecutionException, JsonProcessingException, InterruptedException {
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
 
-        // invoke kafka producer
-        log.info("before sendLibraryEvent");
-//        producer.sendLibraryEvent(libraryEvent);
-        SendResult<Integer, String> sendResult = producer.sendLibraryEventSynchronous(libraryEvent);
-        log.info("SendResult is {}", sendResult);
-        log.info("after sendLibraryEvent");
+        libraryEvent.setType(LibraryEventType.NEW);
+        producer.sendLibraryEventSynchronous(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
+
+
 }
